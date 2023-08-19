4aea7006818057b75761b0972493654d4639011e
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-08-20 00:47:26 +0900

    refactor: テスト用のコメントアウトを消去

--- core/infrastructures/main.mjs
+++ core/infrastructures/main.mjs
@@ -1,7 +1,2 @@
-// import LocalJsonReader from "../adapters/LocalJsonReader.mjs";
-
-// const jsonReader = new LocalJsonReader();
-// const data = await jsonReader.load("/resources/charts/sample.json");
-// console.log(data);
 
 console.log("Hello, world!");
