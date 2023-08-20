9fc580049b9b2356e744321b6b6df4e17ea5ebd5
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-08-20 12:17:16 +0900

    add: UseCaseのsetByJsonを追加

--- core/usecases/ChartUsecase.mjs
+++ core/usecases/ChartUsecase.mjs
@@ -7,4 +7,13 @@ export default class ChartUsecase {
         this.chart = chart;
     }
 
+    setByJSON(json) {
+        this.chart.bpm = json.bpm;
+        this.notes = json.notes;
+    }
+
+    setNotesByString(str) {
+        // TODO: 実装
+        return str
+    }
 }
--- core/usecases/MusicUsecase.mjs
+++ core/usecases/MusicUsecase.mjs
@@ -3,8 +3,15 @@
  */
 
 export default class MusicUsecase {
-    constructor(music) {
+    constructor(music, chartUseCase) {
         this.music = music;
+        this.chartUseCase = chartUseCase;
+        chartUseCase.music = music;
     }
 
+    setByJson(json) {
+        this.music.title = json.title;
+        this.music.file = json.file;
+        this.chartUseCase.setByJson(json);
+    }
 }
